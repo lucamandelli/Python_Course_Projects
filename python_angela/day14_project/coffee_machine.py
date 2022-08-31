5ba8254fbf70cd70c000fab4e9627464664ab0b4
Luca Mandelli <luca.part.mand@gmail.com>
2022-08-31 12:51:56 -0300

    day14_project_new_changes

--- python_angela/day14_project/coffee_machine.py
+++ python_angela/day14_project/coffee_machine.py
@@ -2,32 +2,8 @@ machine_on = True
 water = 100
 milk = 50
 coffee = 76
-money = 2.5
+money = 0.0
 while machine_on:
-    machine_prompt= input("What would you like? \tespresso:1 \tlatte:2\tcappuccino:3\n")
-    if machine_prompt == "off":
-        print("The Coffee Machine is Turning Off...")
-        machine_on = False
-    if machine_prompt == "report":
-        print(f"\nThe resources availabe are: \nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g \nMoney: ${money}\n")
-    if machine_prompt == "maintain":
-        refill = True
-        while refill:
-            more_refill = "yes"
-            which_fill = input("Which one you want to refill: \t1:Water \t2:Milk \t3:Coffee")
-            if which_fill == 1:
-                water_refill = input("How much water in mililiters: ")
-                water = water_refill
-                more_refill = input("Refill another one? type 'yes' or 'no' ")
-            elif which_fill == 2:
-                milk_refill = input("How much milk in mililiters: ")
-                milk = milk_refill
-                more_refill = input("Refill another one? type 'yes' or 'no' ")
-            elif which_fill == 3:
-                coffee_refill = input("How much coffee in grams: ")
-                coffee = coffee_refill
-                more_refill = input("Refill another one? type 'yes' or 'no' ")
-            else:
-                print("ERROR, please type 1, 2 or 3")
-            if more_refill == "no":
-                refill = False
+    machine_prompt= input("What would you like? \tespresso:1 \tlatte:2 \tcappuccino:3\n")    
+    if machine_prompt == "maintainer":
+        maintainer
---
+++ python_angela/day14_project/maintainer.py
@@ -0,0 +1,42 @@
+import os
+
+
+def maintainer():
+    os.system('cls')
+    right_passwd = False
+    password = input("Please type the password for maintainer: ")
+    while not right_passwd:            
+        if password == "987654321":                
+            print("Hello maintainer")                                
+            machine_prompt= int(input("What are you looking for? 1:Report \t2:Refill \t3:Shut Down\n"))                 
+            if machine_prompt == 1:
+                print(f"\nThe resources availabe are: \nWater: {water}ml \nMilk: {milk}ml \nCoffee: {coffee}g \nMoney: ${money}\n")
+            elif machine_prompt == 2:
+                refill = True
+                while refill:
+                    more_refill = "yes"
+                    which_fill = input("Which one you want to refill: \t1:Water \t2:Milk \t3:Coffee\n")
+                    if which_fill == "1":
+                        water_refill = int(input("How much water in mililiters: "))
+                        water += water_refill
+                        more_refill = input("Refill another one? type 'yes' or 'no' ")
+                    elif which_fill == "2":
+                        milk_refill = int(input("How much milk in mililiters: "))
+                        milk += milk_refill
+                        more_refill = input("Refill another one? type 'yes' or 'no' ")
+                    elif which_fill == "3":
+                        coffee_refill = int(input("How much coffee in grams: "))
+                        coffee += coffee_refill
+                        more_refill = input("Refill another one? type 'yes' or 'no' ")
+                    else:
+                        print("ERROR, please type 1, 2 or 3")
+                    if more_refill == "no":
+                        os.system('cls')
+                        refill = False
+            elif machine_prompt == 3:
+                right_passwd = True
+                print("The Coffee Machine is Turning Off...")
+                machine_on = False
+        else:
+            print("\nWrong Password\n")
+            right_passwd = True
