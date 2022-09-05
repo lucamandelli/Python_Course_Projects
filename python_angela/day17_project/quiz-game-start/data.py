2f1d223b04cfb85fc5f0302914095bd00e11ff42
Luca Mandelli <luca.part.mand@gmail.com>
2022-09-05 20:15:32 -0300

    day17_project_done

---
+++ python_angela/day17_project/quiz-game-start/data.py
@@ -0,0 +1,12 @@
+question_data = [
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy", "question": "The main character in the &quot;Half-Life&quot; franchise is named Morgan Freeman.", "correct_answer": "False", "incorrect_answers": ["True"]}, 
+    {"category": "Celebrities", "type": "boolean", "difficulty": "medium", "question": "Michael Jackson had a pet python named &lsquo;Crusher&rsquo;.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy", "question": "There are 6 legendary cards in &quot;Clash Royale&quot;.", "correct_answer": "False", "incorrect_answers": ["True"]}, 
+    {"category": "General Knowledge", "type": "boolean", "difficulty": "medium", "question": "The bikini is named after the &quot;Bikini Atoll&quot;, an island where the United States conducted tests on atomic bombs.", "correct_answer": "True", "incorrect_answers": ["False"]}, 
+    {"category": "Mythology", "type": "boolean", "difficulty": "easy", "question": "According to Greek Mythology, Zeus can control lightning.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Video Games", "type": "boolean", "difficulty": "easy", "question": "In &quot;Undertale&quot;, the main character of the game is Sans.", "correct_answer": "False", "incorrect_answers": ["True"]},
+    {"category": "Science: Mathematics", "type": "boolean", "difficulty": "medium", "question": "The set of all algebraic numbers is countable.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Animals", "type": "boolean", "difficulty": "easy", "question": "Rabbits can see what&#039;s behind themselves without turning their heads.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Geography", "type": "boolean", "difficulty": "easy", "question": "There is an island in Japan called \u014ckunoshima, A.K.A. &quot;Rabbit Island&quot;, so named because of it&#039;s huge population of rabbits.", "correct_answer": "True", "incorrect_answers": ["False"]},
+    {"category": "Entertainment: Japanese Anime & Manga", "type": "boolean", "difficulty": "hard", "question": "The character Plum from &quot;No Game No Life&quot; is a girl.", "correct_answer": "False", "incorrect_answers": ["True"]}
+    ]
